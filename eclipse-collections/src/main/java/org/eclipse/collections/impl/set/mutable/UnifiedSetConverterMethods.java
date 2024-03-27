f1c844126ce8612771c2ab71794cdbb5aa4b4c40
Han Nicolas <nicolas.han.etu@univ-lille.fr>
2024-03-27 11:11:55 +0100

    Grande Modification: décomposition de la god-class UnifiedSet, version finale

--- eclipse-collections/src/main/java/org/eclipse/collections/impl/set/mutable/UnifiedSetConverterMethods.java
+++ eclipse-collections/src/main/java/org/eclipse/collections/impl/set/mutable/UnifiedSetConverterMethods.java
@@ -11,9 +11,24 @@
 package org.eclipse.collections.impl.set.mutable;
 
 import java.lang.reflect.Array;
+import java.util.Iterator;
 import java.util.concurrent.ExecutorService;
 
+import org.eclipse.collections.api.LazyIterable;
+import org.eclipse.collections.api.block.function.Function;
+import org.eclipse.collections.api.block.predicate.Predicate;
+import org.eclipse.collections.api.block.procedure.Procedure;
 import org.eclipse.collections.api.set.ParallelUnsortedSetIterable;
+import org.eclipse.collections.impl.lazy.AbstractLazyIterable;
+import org.eclipse.collections.impl.lazy.parallel.AbstractBatch;
+import org.eclipse.collections.impl.lazy.parallel.AbstractParallelIterable;
+import org.eclipse.collections.impl.lazy.parallel.bag.CollectUnsortedBagBatch;
+import org.eclipse.collections.impl.lazy.parallel.bag.FlatCollectUnsortedBagBatch;
+import org.eclipse.collections.impl.lazy.parallel.bag.UnsortedBagBatch;
+import org.eclipse.collections.impl.lazy.parallel.set.AbstractParallelUnsortedSetIterable;
+import org.eclipse.collections.impl.lazy.parallel.set.RootUnsortedSetBatch;
+import org.eclipse.collections.impl.lazy.parallel.set.SelectUnsortedSetBatch;
+import org.eclipse.collections.impl.lazy.parallel.set.UnsortedSetBatch;
 
 public class UnifiedSetConverterMethods<T>
 {
@@ -107,6 +122,61 @@ public class UnifiedSetConverterMethods<T>
     }
 
 
+    private final class UnifiedUnsortedSetBatch extends AbstractBatch<T> implements RootUnsortedSetBatch<T>
+    {
+        private final int chunkStartIndex;
+        private final int chunkEndIndex;
+
+        private UnifiedUnsortedSetBatch(int chunkStartIndex, int chunkEndIndex)
+        {
+            this.chunkStartIndex = chunkStartIndex;
+            this.chunkEndIndex = chunkEndIndex;
+        }
+
+        @Override
+        public void forEach(Procedure<? super T> procedure)
+        {
+            UnifiedSetConverterMethods.this.unifiedSet.forEachMethods.each(procedure, this.chunkStartIndex, this.chunkEndIndex);
+        }
+
+        @Override
+        public boolean anySatisfy(Predicate<? super T> predicate)
+        {
+            return UnifiedSetConverterMethods.this.unifiedSet.shortCircuit(predicate, true, true, false, this.chunkStartIndex, this.chunkEndIndex);
+        }
+
+        @Override
+        public boolean allSatisfy(Predicate<? super T> predicate)
+        {
+            return UnifiedSetConverterMethods.this.unifiedSet.shortCircuit(predicate, false, false, true, this.chunkStartIndex, this.chunkEndIndex);
+        }
+
+        @Override
+        public T detect(Predicate<? super T> predicate)
+        {
+            return UnifiedSetConverterMethods.this.unifiedSet.detect(predicate, this.chunkStartIndex, this.chunkEndIndex);
+        }
+
+        @Override
+        public UnsortedSetBatch<T> select(Predicate<? super T> predicate)
+        {
+            return new SelectUnsortedSetBatch<>(this, predicate);
+        }
+
+        @Override
+        public <V> UnsortedBagBatch<V> collect(Function<? super T, ? extends V> function)
+        {
+            return new CollectUnsortedBagBatch<>(this, function);
+        }
+
+        @Override
+        public <V> UnsortedBagBatch<V> flatCollect(Function<? super T, ? extends Iterable<V>> function)
+        {
+            return new FlatCollectUnsortedBagBatch<>(this, function);
+        }
+    }
+
+
     private int copyBucketToArray(Object[] result, UnifiedSet.ChainedBucket bucket, int count)
     {
         do
@@ -152,6 +222,135 @@ public class UnifiedSetConverterMethods<T>
             result[size] = null;
         }
         return result;
+    }
+
+
+    public ParallelUnsortedSetIterable<T> asParallel(ExecutorService executorService, int batchSize)
+    {
+        if (executorService == null)
+        {
+            throw new NullPointerException();
+        }
+        if (batchSize < 1)
+        {
+            throw new IllegalArgumentException();
+        }
+        return new UnifiedSetConverterMethods.UnifiedSetParallelUnsortedIterable(executorService, batchSize);
+    }
+
+
+    private final class UnifiedSetParallelUnsortedIterable extends AbstractParallelUnsortedSetIterable<T, RootUnsortedSetBatch<T>>
+    {
+        private final ExecutorService executorService;
+        private final int batchSize;
+
+        private UnifiedSetParallelUnsortedIterable(ExecutorService executorService, int batchSize)
+        {
+            this.executorService = executorService;
+            this.batchSize = batchSize;
+        }
+
+        @Override
+        public ExecutorService getExecutorService()
+        {
+            return this.executorService;
+        }
+
+        @Override
+        public int getBatchSize()
+        {
+            return this.batchSize;
+        }
+
+        @Override
+        public LazyIterable<RootUnsortedSetBatch<T>> split()
+        {
+            return new UnifiedSetConverterMethods.UnifiedSetParallelUnsortedIterable.UnifiedSetParallelSplitLazyIterable();
+        }
+
+        @Override
+        public void forEach(Procedure<? super T> procedure)
+        {
+            AbstractParallelIterable.forEach(this, procedure);
+        }
+
+        @Override
+        public boolean anySatisfy(Predicate<? super T> predicate)
+        {
+            return AbstractParallelIterable.anySatisfy(this, predicate);
+        }
+
+        @Override
+        public boolean allSatisfy(Predicate<? super T> predicate)
+        {
+            return AbstractParallelIterable.allSatisfy(this, predicate);
+        }
+
+        @Override
+        public T detect(Predicate<? super T> predicate)
+        {
+            return AbstractParallelIterable.detect(this, predicate);
+        }
+
+        @Override
+        public Object[] toArray()
+        {
+            // TODO: Implement in parallel
+            return UnifiedSetConverterMethods.this.toArray();
+        }
+
+        @Override
+        public <E> E[] toArray(E[] array)
+        {
+            // TODO: Implement in parallel
+            return UnifiedSetConverterMethods.this.toArray(array);
+        }
+
+        private class UnifiedSetParallelSplitIterator implements Iterator<UnifiedUnsortedSetBatch>
+        {
+            protected int chunkIndex;
+
+            @Override
+            public boolean hasNext()
+            {
+                return this.chunkIndex * UnifiedSetConverterMethods.UnifiedSetParallelUnsortedIterable.this.batchSize < UnifiedSetConverterMethods.this.unifiedSet.table.length;
+            }
+
+            @Override
+            public UnifiedUnsortedSetBatch next()
+            {
+                int chunkStartIndex = this.chunkIndex * UnifiedSetConverterMethods.UnifiedSetParallelUnsortedIterable.this.batchSize;
+                int chunkEndIndex = (this.chunkIndex + 1) * UnifiedSetConverterMethods.UnifiedSetParallelUnsortedIterable.this.batchSize;
+                int truncatedChunkEndIndex = Math.min(chunkEndIndex, UnifiedSetConverterMethods.this.unifiedSet.table.length);
+                this.chunkIndex++;
+                return new UnifiedSetConverterMethods.UnifiedUnsortedSetBatch(chunkStartIndex, truncatedChunkEndIndex);
+            }
+
+            @Override
+            public void remove()
+            {
+                throw new UnsupportedOperationException("Cannot call remove() on " + this.getClass().getSimpleName());
+            }
+        }
+
+        private class UnifiedSetParallelSplitLazyIterable
+                extends AbstractLazyIterable<RootUnsortedSetBatch<T>>
+        {
+            @Override
+            public void each(Procedure<? super RootUnsortedSetBatch<T>> procedure)
+            {
+                for (RootUnsortedSetBatch<T> chunk : this)
+                {
+                    procedure.value(chunk);
+                }
+            }
+
+            @Override
+            public Iterator<RootUnsortedSetBatch<T>> iterator()
+            {
+                return new UnifiedSetConverterMethods.UnifiedSetParallelUnsortedIterable.UnifiedSetParallelSplitIterator();
+            }
+        }
     }
 
 
